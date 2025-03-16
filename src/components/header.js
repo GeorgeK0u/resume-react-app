9583da4860d5130245bfcc7eae2e3575ed7eba4b
GeorgeK0u <144594716+GeorgeK0u@users.noreply.github.com>
2025-03-16 14:46:41 +0200

    Removed navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,13 @@
 import '../assets/styles/helper.css'
 import '../assets/styles/header.css'
-import { is_nav_item_active, go_to } from '../utils/helper.js'
 
 function Header()
 {
 	return (
 		<header>
-			<nav className='nav-bar'>
-				<button className='header-nav-item' onClick={() => go_to('/')} disabled={is_nav_item_active('/')}>Home</button>
-			</nav>
+			<div id='header-logo-wrapper'>
+				<span id='header-logo'>ΓΚ</span>
+			</div>
 		</header>
 	);
 }
